5970326a0b23e41ccc7ac1f22bc9d31a8a75d7fe
Nisarg <nisargvaghela2001@gmail.com>
2024-04-07 20:24:30 +0530

    added build folder

--- server.js
+++ server.js
@@ -5,17 +5,23 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 const dbConfig = require("./config/dbConfig");
 const bodyParser = require("body-parser");
+const path = require("path");
+
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
+// app.use(express.static(__dirname + '/public'));
 
 app.use("/api/users", require("./routes/usersRoutes"));
 app.use("/api/buses", require("./routes/busesRoutes"));
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/bookings", require("./routes/bookingsRoutes"));
 app.use("/api/cities", require("./routes/citiesRoutes"));
-
+app.use(express.static('build'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
 // listen to port
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
